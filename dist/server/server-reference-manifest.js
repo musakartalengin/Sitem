0c3ebe47f05ec1d653ec06a72a4a3c85ed161bc8
musakartalengin <69765469+musakartalengin@users.noreply.github.com>
2024-12-27 20:03:38 +0300

    Update build configuration for Netlify

---
+++ dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FazRTrvBJPpkhxzarLbW15YkcNzh1k3fobr6wa5J0Ps=\"\n}"
--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
+  distDir: 'dist',
 }
 
 module.exports = nextConfig 
